34f814ec98f68b42d4501909da274b059bb67329
Alvaro5801 <75311782+alvaro5801@users.noreply.github.com>
2025-03-23 17:24:54 -0300

    Update FornecedorController.java

--- JavaAvanadeProject-1/src/main/java/lojas/estoque/controller/FornecedorController.java
+++ JavaAvanadeProject-1/src/main/java/lojas/estoque/controller/FornecedorController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/fornecedores")
+@RequestMapping("/api/fornecedores")
 public class FornecedorController {
 
     private final FornecedorRepository fornecedorRepository;

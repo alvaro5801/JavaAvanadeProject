1d006150ae60ac41dceed497c16557aedc2421da
Alvaro5801 <75311782+alvaro5801@users.noreply.github.com>
2025-03-23 00:10:37 -0300

    Add files via upload

---
+++ JavaAvanadeProject-1/src/main/java/lojas/estoque/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package lojas.estoque.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro: Já existe um registro com este valor único no banco.");
+    }
+
+    
+}

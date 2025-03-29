c372b6f964a3a37b7e3c56f4393953242b9f9890
Alvaro5801 <75311782+alvaro5801@users.noreply.github.com>
2025-03-29 11:40:42 -0300

    Update CategoriaRepository.java

--- JavaAvanadeProject-1/src/main/java/lojas/estoque/repository/CategoriaRepository.java
+++ JavaAvanadeProject-1/src/main/java/lojas/estoque/repository/CategoriaRepository.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import lojas.estoque.model.Categoria;
+import lojas.estoque.model.Fornecedor;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
     Optional<Categoria> findByNome(String nome);
     boolean existsByNome(String nome);
+    boolean existsByFornecedor(Fornecedor fornecedor);
 }

cb06bb3cd464b070c62a5e4220e727fde23f6f4a
Alvaro5801 <75311782+alvaro5801@users.noreply.github.com>
2025-03-29 14:13:27 -0300

    Update Fornecedor.java

--- JavaAvanadeProject-1/src/main/java/lojas/estoque/model/Fornecedor.java
+++ JavaAvanadeProject-1/src/main/java/lojas/estoque/model/Fornecedor.java
@@ -2,16 +2,9 @@ package lojas.estoque.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -32,13 +25,16 @@ public class Fornecedor {
     @JsonIgnore
     private List<Produto> produtos;
 
+    // ✅ Adicionado: categorias pertencentes a este fornecedor
+    @OneToMany(mappedBy = "fornecedor", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<Categoria> categorias;
+
     public Fornecedor() {}
 
     public Fornecedor(String nome) {
         this.nome = nome;
     }
-
-    
 
     public Long getId() {
         return id;
@@ -62,5 +58,13 @@ public class Fornecedor {
 
     public void setProdutos(List<Produto> produtos) {
         this.produtos = produtos;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
     }
 }

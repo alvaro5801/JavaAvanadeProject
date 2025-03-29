5b47c51f3430e6df9b3770482dd3219a124f329e
Alvaro5801 <75311782+alvaro5801@users.noreply.github.com>
2025-03-29 11:44:33 -0300

    Update ProdutoRepository.java

--- JavaAvanadeProject-1/src/main/java/lojas/estoque/repository/ProdutoRepository.java
+++ JavaAvanadeProject-1/src/main/java/lojas/estoque/repository/ProdutoRepository.java
@@ -1,16 +1,20 @@
 package lojas.estoque.repository;
 
 import lojas.estoque.model.Produto;
+import lojas.estoque.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
     boolean existsByNome(String nome);
+    boolean existsByCategoria(Categoria categoria); 
 
     @Transactional
     @Modifying
